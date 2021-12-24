0bc8a4b4d64d17699cc732b15476b73e08707c24
bayu <cbzvdani@gmail.com>
2021-12-24 11:46:09 +0700

    fix

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const UserRoute = require('./routes/users');
 const PostRoute = require('./routes/posts');
+const CategoryRoute = require('./routes/categories');
+const multer = require('multer');
 dotenv.config(); //init .env
 app.use(express.json()); //able to send json over body
 mongoose.connect(process.env.MONGO_URL, {
         connectTimeoutMS: 1000
     }).then(console.log("connected"))
     .catch((err) => console.log(err))
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+})
+const upload = multer({ storage: storage });
 
+app.post("/api/uploads", upload.single("file"), (req, res) => {
+    res.status(200).json("success uploaded");
+});
 app.use("/api/auth", authRouter)
 app.use("/api/users", UserRoute)
 app.use("/api/posts", PostRoute)
+app.use("/api/categories", CategoryRoute)
 
 app.listen("5000", () => {
     console.log("running")
